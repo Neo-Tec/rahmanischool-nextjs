dbdd43070851208ab5bc18b7ac9570b54ea6e7ac
VirtualPirate <artaza13020artaza@gmail.com>
2024-03-17 15:55:45 +0530

    Added teachers schema

--- lib/sanity/schemas/index.ts
+++ lib/sanity/schemas/index.ts
@@ -5,14 +5,15 @@ import author from "./author";
 import settings from "./settings";
 import homepage from "./homepage";
 
-import team from "./team";
-// import { team_page } from "./page/team_page";
+import teachers from "./teachers";
+import { team_page } from "./page/_unused/team_page";
 // import { about_page } from "./page/about_page";
 
 export const schemaTypes = [
   // post,
   // category,
   settings,
+  teachers,
   // blockContent,
   // team,
   // Page Data
