99e713a6bd75d992b9cd1963381119f1e2b3b3f5
VirtualPirate <artaza13020artaza@gmail.com>
2024-03-17 16:29:16 +0530

    Completed the blog section

---
+++ src/app/(website)/blog/page.tsx
@@ -0,0 +1,224 @@
+import { Footer } from "@/components/footer";
+import { Nav } from "@/components/nav";
+import { TopBar } from "@/components/topbar";
+
+/* eslint-disable @next/next/no-img-element */
+export default function Blog() {
+  return (
+    <>
+      <div>
+        <TopBar />
+        <Nav />
+        <div className="all-title-box">
+          <div className="container text-center">
+            <h1>
+              Blog
+              <span className="m_1">
+                Lorem Ipsum dolroin gravida nibh vel velit.
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div id="overviews" className="section wb">
+          <div className="container">
+            <div className="section-title row text-center">
+              <div className="col-md-8 offset-md-2">
+                <p className="lead" />
+              </div>
+            </div>
+            {/* end title */}
+            <hr className="invis" />
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="blog-item">
+                  <div className="image-blog">
+                    <img src="images/bk1.jpg " alt="" className="img-fluid" />
+                  </div>
+                  <div className="meta-info-blog">
+                    <span>
+                      <i className="fa fa-calendar" />{" "}
+                      <a href="#">May 11, 2015</a>{" "}
+                    </span>
+                  </div>
+                  <div className="blog-title">
+                    <h2>
+                      <a href="#" title="">
+                        perferendis doloribus asperiores.
+                      </a>
+                    </h2>
+                  </div>
+                  <div className="blog-desc">
+                    <p>
+                      Lorem ipsum door sit amet, fugiat deicata avise id cum, no
+                      quo maiorum intel ogrets geuiat operts elicata libere
+                      avisse id cumlegebat, liber regione eu sit....{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* end col */}
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="blog-item">
+                  <div className="image-blog">
+                    <img src="images/bk2.jpg" alt="" className="img-fluid" />
+                  </div>
+                  <div className="meta-info-blog">
+                    <span>
+                      <i className="fa fa-calendar" />{" "}
+                      <a href="#">May 11, 2015</a>{" "}
+                    </span>
+                  </div>
+                  <div className="blog-title">
+                    <h2>
+                      <a href="#" title="">
+                        perferendis doloribus asperiores.
+                      </a>
+                    </h2>
+                  </div>
+                  <div className="blog-desc">
+                    <p>
+                      Lorem ipsum door sit amet, fugiat deicata avise id cum, no
+                      quo maiorum intel ogrets geuiat operts elicata libere
+                      avisse id cumlegebat, liber regione eu sit....{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* end col */}
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="blog-item">
+                  <div className="image-blog">
+                    <img src="images/bk3.jpg" alt="" className="img-fluid" />
+                  </div>
+                  <div className="meta-info-blog">
+                    <span>
+                      <i className="fa fa-calendar" />{" "}
+                      <a href="#">May 11, 2015</a>{" "}
+                    </span>
+                  </div>
+                  <div className="blog-title">
+                    <h2>
+                      <a href="#" title="">
+                        perferendis doloribus asperiores.
+                      </a>
+                    </h2>
+                  </div>
+                  <div className="blog-desc">
+                    <p>
+                      Lorem ipsum door sit amet, fugiat deicata avise id cum, no
+                      quo maiorum intel ogrets geuiat operts elicata libere
+                      avisse id cumlegebat, liber regione eu sit....{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* end col */}
+            </div>
+            {/* end row */}
+            <hr className="hr3" />
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="blog-item">
+                  <div className="image-blog">
+                    <img src="images/bk4.jpg" alt="" className="img-fluid" />
+                  </div>
+                  <div className="meta-info-blog">
+                    <span>
+                      <i className="fa fa-calendar" />{" "}
+                      <a href="#">May 11, 2015</a>{" "}
+                    </span>
+                  </div>
+                  <div className="blog-title">
+                    <h2>
+                      <a href="#" title="">
+                        perferendis doloribus asperiores.
+                      </a>
+                    </h2>
+                  </div>
+                  <div className="blog-desc">
+                    <p>
+                      Lorem ipsum door sit amet, fugiat deicata avise id cum, no
+                      quo maiorum intel ogrets geuiat operts elicata libere
+                      avisse id cumlegebat, liber regione eu sit....{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* end col */}
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="blog-item">
+                  <div className="image-blog">
+                    <img src="images/bk6.jpg" alt="" className="img-fluid" />
+                  </div>
+                  <div className="meta-info-blog">
+                    <span>
+                      <i className="fa fa-calendar" />{" "}
+                      <a href="#">May 11, 2015</a>{" "}
+                    </span>
+                  </div>
+                  <div className="blog-title">
+                    <h2>
+                      <a href="#" title="">
+                        perferendis doloribus asperiores.
+                      </a>
+                    </h2>
+                  </div>
+                  <div className="blog-desc">
+                    <p>
+                      Lorem ipsum door sit amet, fugiat deicata avise id cum, no
+                      quo maiorum intel ogrets geuiat operts elicata libere
+                      avisse id cumlegebat, liber regione eu sit....{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* end col */}
+              <div className="col-lg-4 col-md-6 col-12">
+                <div className="blog-item">
+                  <div className="image-blog">
+                    <img src="images/bk8.jpg" alt="" className="img-fluid" />
+                  </div>
+                  <div className="meta-info-blog">
+                    <span>
+                      <i className="fa fa-calendar" />{" "}
+                      <a href="#">May 11, 2015</a>{" "}
+                    </span>
+                  </div>
+                  <div className="blog-title">
+                    <h2>
+                      <a href="#" title="">
+                        perferendis doloribus asperiores.
+                      </a>
+                    </h2>
+                  </div>
+                  <div className="blog-desc">
+                    <p>
+                      Lorem ipsum door sit amet, fugiat deicata avise id cum, no
+                      quo maiorum intel ogrets geuiat operts elicata libere
+                      avisse id cumlegebat, liber regione eu sit....{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* end col */}
+            </div>
+            {/* end row */}
+          </div>
+          {/* end container */}
+        </div>
+        {/* end section */}
+        <div className="parallax section dbcolor">
+          <div className="container">
+            <div className="row logos"></div>
+            {/* end row */}
+          </div>
+          {/* end container */}
+        </div>
+        <Footer />
+        <a href="#" id="scroll-to-top" className="dmtop global-radius">
+          <i className="fa fa-angle-up" />
+        </a>
+      </div>
+    </>
+  );
+}
